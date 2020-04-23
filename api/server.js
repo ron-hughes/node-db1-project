f942d9ebefd9d62b5119a1e23e5c6fac9145625d
U-THEOPHILUS\Ron <ronald.hughes1982@gmail.com>
2020-04-22 21:15:33 -0400

    updated project with knex commands

--- api/server.js
+++ api/server.js
@@ -1,9 +1,8 @@
 const express = require("express");
-
-const db = require("../data/dbConfig.js");
-
+const accountRouter = require("../router/accountRouter.js")
 const server = express();
 
 server.use(express.json());
+server.use("/api/accounts", accountRouter)
 
 module.exports = server;

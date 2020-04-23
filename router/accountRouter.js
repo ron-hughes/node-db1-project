60b28f69cbfa94e636d6cbdfdee197168cbdf472
U-THEOPHILUS\Ron <ronald.hughes1982@gmail.com>
2020-04-23 09:35:52 -0400

    added query string logic

---
+++ router/accountRouter.js
@@ -0,0 +1,53 @@
+const express = require("express")
+const db = require("../data/dbConfig.js");
+const router = express.Router();
+
+
+// GET ALL ACCOUNTS
+router.get("/", async(req, res, next) => {
+    const { limit, sortby, sortdir } = req.query
+  
+    
+    if (limit || sortby || sortdir) {
+        const queried = await db.select("*").from("accounts").limit(limit)
+        res.json(queried)
+    }
+    else {
+    try {
+        const accounts = await db.select("*").from("accounts")
+        res.json(accounts)
+    }
+    catch (err) {
+        next(err)
+    }
+}
+})
+
+// GET ACCOUNT BY ID
+router.get("/:id", async(req, res, next) => {
+
+    try {
+        const account = await db("accounts").where("id", req.params.id);
+        res.json(account)
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
+// DELETE ACCOUNT BY ID
+router.delete("/:id", async(req, res, next) => {
+    const { id } = req.params
+
+    try {
+        await db("accounts").where("id", id).del()
+        res.status(204).end()
+    }
+    catch (err) {
+        next(err)
+    }
+
+})
+
+
+module.exports = router
